4fe2a11a66adaa5340036ef11f52fe09cb0226de
Kkickp <sakinahdwiagus@gmail.com>
2026-01-14 10:18:57 +0700

    update

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,10 @@
-  <script src="/admin_template/js/sb-admin-2.min.js"></script>
+</div>
+</div>
+</div>
+
+<script src="/admin_template/vendor/jquery/jquery.min.js"></script>
+<script src="/admin_template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/admin_template/js/sb-admin-2.min.js"></script>
+
 </body>
 </html>
