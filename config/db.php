024f2cd36f766995ab95a74b52e6082c991f031c
Kkickp <sakinahdwiagus@gmail.com>
2026-01-14 09:16:08 +0700

    Fix admin panel DB to TiDB SSL

--- config/db.php
+++ config/db.php
@@ -1,5 +1,17 @@
 <?php
-$conn = new mysqli("localhost","root","","blangkis_admin");
-if($conn->connect_error) die("DB Error");
-session_start();
-?>
+
+$host = "gateway01.ap-northeast-1.prod.aws.tidbcloud.com";
+$user = "2YwRN5U9FGjWBPP.root";
+$pass = "bHhYbhEX4G1IpYKn";
+$db   = "test";
+$port = 4000;
+
+$ssl_ca = __DIR__ . "/../certs/ca.pem";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$conn = mysqli_init();
+$conn->ssl_set(NULL, NULL, $ssl_ca, NULL, NULL);
+$conn->real_connect($host, $user, $pass, $db, $port);
+
+$conn->set_charset("utf8mb4");
